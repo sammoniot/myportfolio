a5fa003a15a29646c0e4635b63560527e91c5a6c
Samantha M <sam7694@psu.edu>
2024-10-23 14:57:58 -0400

    added javascript files

---
+++ docs/javascript/javascript1.js
@@ -0,0 +1,88 @@
+window.addEventListener('DOMContentLoaded', bunny, false);
+
+function bunny() {
+    alert ('Hi there! Looks like the page loaded! Yay!');
+    var buttons = document.getElementsByTagName("button")
+    buttons[0].addEventListener('click', changeColor, false);
+    buttons[1].addEventListener('click', dodgerInfo, false);
+    buttons[2].addEventListener('click', newFunction, false);
+    buttons[3].addEventListener('click', anotherFunction, false);
+    buttons[4].addEventListener('click', aDifferentFunction, false);
+    buttons[5].addEventListener('click', aFourthFunction, false);
+    buttons[6].addEventListener('click', aFifthFunction, false);
+    buttons[6].addEventListener('click', aSixthFunction, false);}
+    function changeColor() {
+    var p1 = document.getElementById("colorToggle")
+    {
+        p1.style.backgroundColor = "DodgerBlue";
+    }
+    }
+    function dodgerInfo() {
+    alert('My favorite team is the Los Angeles Dodgers! They will win the World Series this year!')
+    var pars = document.getElementId("team")
+    for (var i = 0, length = pars.length; i < length; i++) {
+        pars[i].style.backgroundColor = "DodgerBlue";
+    }
+}
+    function newFunction(){  
+    alert ('I have an older brother named Charlie and a dog named Sandy');
+    var pars = document.getElementById("family")
+    for (var i = 0, length = li.length; i < length; i++) {
+        li[i].style.backgroundColor = "pink";
+    }
+}
+    function anotherFunction(){
+    alert('17')
+     
+     var li = document.getElementById("mvp") 
+     li.innerHTML = "17"; 
+     for (var i = 0, length = li.length; i < length; i++){
+     li[i].style.backgroundColor = "skyblue"    
+     }
+     }
+     function aDifferentFunction(){
+     alert('13')
+     
+     var li = document.getElementById("max") 
+     li.innerHTML = "13"; 
+     for (var i = 0, length = li.length; i < length; i++){
+     li[i].style.backgroundColor = "skyblue" 
+     }
+     }
+      function aDifferentFunction(){
+     alert('13')
+     
+     var li = document.getElementById("max") 
+     li.innerHTML = "13"; 
+     for (var i = 0, length = li.length; i < length; i++){
+     li[i].style.backgroundColor = "skyblue" 
+     } 
+    }
+    function aFourthFunction(){
+      alert('16')
+     
+     var li = document.getElementById("will") 
+     li.innerHTML = "16"; 
+     for (var i = 0, length = li.length; i < length; i++){
+     li[i].style.backgroundColor = "skyblue" 
+     }   
+    }
+     function aFifthFunction(){
+      alert('5')
+     
+     var li = document.getElementById("free") 
+     li.innerHTML = "5"; 
+     for (var i = 0, length = li.length; i < length; i++){
+     li[i].style.backgroundColor = "skyblue" 
+     }   
+    }
+     function aSixthFunction(){
+      alert('37')
+     
+     var li = document.getElementById("teo") 
+     li.innerHTML = "37"; 
+     for (var i = 0, length = li.length; i < length; i++){
+     li[i].style.backgroundColor = "skyblue" 
+     }   
+    }
+    
